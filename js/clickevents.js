408e177dc2335a3a3a0061a6af6ade5afbea321b
photozynthesis <photozynthesis@outlook.com>
2019-05-17 08:41:10 +0800

    update 5.17

--- js/clickevents.js
+++ js/clickevents.js
@@ -13,6 +13,7 @@ function changeWallpaper() {
 function showPage(id) {
     if (id == page) {
         document.getElementById("content").innerHTML = '<object type="text/html" data="html/' + id + '.html" width="100%" height="100%"></object>';
+        // $('#content').load('html/' + id + '.html');
         return;
     }
     if (page != null) {
@@ -23,6 +24,7 @@ function showPage(id) {
     document.getElementById(id).style.backgroundColor = '#333333';
     document.getElementById(id).getElementsByTagName("a")[0].style.color = '#dddddd';
     document.getElementById("content").innerHTML = '<object type="text/html" data="html/' + id + '.html" width="100%" height="100%"></object>';
+    // $('#content').load('html/' + id + '.html');
 }
 
 function toTop() {
